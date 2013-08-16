fd5be3a57c20020c8c00df750501cba51b6f66fa
Masshhka <mashka-life@mail.ru>
2013-08-16 13:16:01 +0400

    db

--- src/main/java/com/mycompany/mv_pr/DB.java
+++ src/main/java/com/mycompany/mv_pr/DB.java
@@ -13,70 +13,51 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Properties;
 
-
 /**
  *
  * @author user
  */
 public class DB {
     /* the default framework is embedded*/
+
     private String framework = "embedded";
     private String driver = "org.apache.derby.jdbc.EmbeddedDriver";
     private String protocol = "jdbc:derby:";
 
-    void go(String[] args) throws SQLException
-    {
+    void go(String[] args) throws SQLException {
         parseArguments(args);
         System.out.println("SimpleApp starting in " + framework + " mode");
-        loadDriver();
         Connection conn = null;
         ArrayList<Statement> statements = new ArrayList<Statement>(); // list of Statements, PreparedStatements
         PreparedStatement psInsert = null;
         PreparedStatement psUpdate = null;
         Statement s = null;
         ResultSet rs = null;
-        try
+        try {
+        } catch (Exception e) {
+        }
         {
             Properties props = new Properties(); // connection properties
             props.put("user", "user1");
             props.put("password", "user1");
             String dbName = "derbyDB"; // the name of the database
-            conn = DriverManager.getConnection(protocol + dbName
-                    + ";create=true", props);
+            conn = DriverManager.getConnection(protocol + dbName + ";create=true", props);
             System.out.println("Connected to and created database " + dbName);
             conn.setAutoCommit(false);
-
             s = conn.createStatement();
             statements.add(s);
             s.execute("create table location(num int, addr varchar(40))");
             System.out.println("Created table location");
             conn.commit();
             System.out.println("Committed the transaction");
-			            try {
-                if (conn != null) {
-                    conn.close();
-                    conn = null;
-                }
-            } catch (SQLException sqle) {
-                printSQLException(sqle);
-            }
-    
-        private void loadDriver() {
+        }
         try {
-            Class.forName(driver).newInstance();
-            System.out.println("Loaded the appropriate driver");
-        } catch (ClassNotFoundException cnfe) {
-            System.err.println("\nUnable to load the JDBC driver " + driver);
-            System.err.println("Please check your CLASSPATH.");
-            cnfe.printStackTrace(System.err);
-        } catch (InstantiationException ie) {
-            System.err.println(
-                        "\nUnable to instantiate the JDBC driver " + driver);
-            ie.printStackTrace(System.err);
-        } catch (IllegalAccessException iae) {
-            System.err.println(
-                        "\nNot allowed to access the JDBC driver " + driver);
-            iae.printStackTrace(System.err);
+            if (conn != null) {
+                conn.close();
+                conn = null;
+            }
+        } catch (SQLException sqle) {
+            printSQLException(sqle);
         }
     }
 
@@ -88,4 +69,3 @@ public class DB {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
-    
