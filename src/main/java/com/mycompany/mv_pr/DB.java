5ddc625494682fd8bc1bfaa95c0b534b35071fe8
Masshhka <mashka-life@mail.ru>
2013-08-21 15:36:46 +0400

    NEW

--- src/main/java/com/mycompany/mv_pr/DB.java
+++ src/main/java/com/mycompany/mv_pr/DB.java
@@ -14,68 +14,67 @@ import java.util.ArrayList;
 import java.util.Properties;
 
 /**
- * 
+ *
  * @author user
  */
 public class DB {
-	/* the default framework is embedded */
+    /* the default framework is embedded */
 
-	private String framework = "embedded";
-	private String protocol = "jdbc:derby:";
-	private static final String ERROR_TABLE_EXISTS_CODE = "X0Y32";
-	void go(String[] args) throws SQLException {
-		System.out.println("DB starting in " + framework + " mode");
-		System.out.println("SimpleApp starting in " + framework + " mode");
-		Connection conn = null;
-		ArrayList<Statement> statements = new ArrayList<Statement>(); // list of
-		PreparedStatement psInsert = null;
-		// PreparedStatement psUpdate = null;
-		Statement s = null;
-		ResultSet rs = null;
-		Properties props = new Properties(); // connection properties
-		String dbName = "location"; // the name of the database
-		conn = DriverManager.getConnection(protocol + dbName + ";create=true",props);
-		System.out.println("Connected to and created database " + dbName);
-		//conn.setAutoCommit(false);
-		s = conn.createStatement();
-		statements.add(s);
-		try {
-			s.execute("create table location(id int, param_name varchar(40), param_value varchar(40), app_id int");
-		} catch (SQLException sqlException) {
-			if (!sqlException.getSQLState().equals(ERROR_TABLE_EXISTS_CODE)) {
-				throw sqlException;
-			}
-		}
-		System.out.println("Created table location");
-		//conn.commit();
-		System.out.println("Committed the transaction");
+    private String framework = "embedded";
+    private String protocol = "jdbc:derby:";
+    private static final String ERROR_TABLE_EXISTS_CODE = "X0Y32";
 
-		psInsert = conn.prepareStatement("insert into location values (?, ?, ?, ?)");
-		statements.add(psInsert);
-		psInsert.setInt(1, 1);
-                psInsert.setString(2, "SERVER_NAME");
-		psInsert.setString(3, Launcher.SERVER_NAME);
-                psInsert.setInt(4, 1);
-		psInsert.executeUpdate();
-		rs = s.executeQuery("select * from location");
-		while (rs.next()) {
-		System.out.println(rs.getInt("id") + " " + rs.getString("param_name")+ " " + rs.getString("param_value")+ " " + rs.getInt("app_id"));
-		}
-		//conn.commit();
-                psInsert.close();
-		s.close();
-		
-		try {
-			if (conn != null) {
-				conn.close();
-				conn = null;
-			}
-		} catch (SQLException sqle) {
-			sqle.printStackTrace();
-		}
-	}
+    void go(String[] args) throws SQLException {
+        System.out.println("DB starting in " + framework + " mode");
+        System.out.println("SimpleApp starting in " + framework + " mode");
+        Connection conn = null;
+        ArrayList<Statement> statements = new ArrayList<Statement>(); // list of
+        PreparedStatement psInsert = null;
+        // PreparedStatement psUpdate = null;
+        Statement s = null;
+        ResultSet rs = null;
+        Properties props = new Properties(); // connection properties
+        String dbName = "location"; // the name of the database
+        conn = DriverManager.getConnection(protocol + dbName + ";create=true", props);
+        System.out.println("Connected to and created database " + dbName);
+        //conn.setAutoCommit(false);
+        s = conn.createStatement();
+        statements.add(s);
+        try {
+            s.execute("create table location(id int, param_name varchar(40), param_value varchar(40), app_id int");
+        } catch (SQLException sqlException) {
+            if (!sqlException.getSQLState().equals(ERROR_TABLE_EXISTS_CODE)) {
+                throw sqlException;
+            }
+        }
+        System.out.println("Created table location");
+        //conn.commit();
+        System.out.println("Committed the transaction");
 
-																	// change
-																	// Templates.
-	
+        psInsert = conn.prepareStatement("insert into location values (?, ?, ?, ?)");
+        statements.add(psInsert);
+        psInsert.setInt(1, 1);
+        psInsert.setString(2, "SERVER_NAME");
+        psInsert.setString(3, Launcher.SERVER_NAME);
+        psInsert.setInt(4, 1);
+        psInsert.executeUpdate();
+        rs = s.executeQuery("select * from location");
+        while (rs.next()) {
+            System.out.println(rs.getInt("id") + " " + rs.getString("param_name") + " " + rs.getString("param_value") + " " + rs.getInt("app_id"));
+        }
+        //conn.commit();
+        psInsert.close();
+        s.close();
+
+        try {
+            if (conn != null) {
+                conn.close();
+                conn = null;
+            }
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+        }
+    }
+    // change
+    // Templates.
 }
