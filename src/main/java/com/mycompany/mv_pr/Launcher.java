a102bf200338332ae1b78c2ebae2938f5b509ca8
Masshhka <mashka-life@mail.ru>
2013-08-21 15:31:20 +0400

    jkhjkj

--- src/main/java/com/mycompany/mv_pr/Launcher.java
+++ src/main/java/com/mycompany/mv_pr/Launcher.java
@@ -21,10 +21,11 @@ import org.mortbay.jetty.webapp.WebAppContext;
 public class Launcher {
 
 //    private static final String APPLICATION_PATH = "src\\main\\webapp";        //путь к папке с приложением было src/main/webapp
-	private static final String APPLICATION_PATH = "src" + File.separator + "main" + File.separator + "webapp";
+    private static final String APPLICATION_PATH = "src" + File.separator + "main" + File.separator + "webapp";
     public static final int SERVER_PORT = 8089;                              // на каком порту работает приложение
     private static final String MAPPING = "/";                               //на каком адресе отвечает приложение
-public static final String SERVER_NAME = "localhost";
+    public static final String SERVER_NAME = "localhost";
+
     private static void initializeServer(String appPath, String mapping, int port) throws Exception {
         Server server = new Server();
         Connector connector = new SelectChannelConnector();
@@ -35,22 +36,19 @@ public static final String SERVER_NAME = "localhost";
         server.start();
         //new Handler[] { root } - тут создаётся массив экземпляров класса Handler с одним элементом - root
     }
+
     public static void main(String[] args) {
-        try 
-        {
+        try {
             try {
                 initializeServer(APPLICATION_PATH, MAPPING, SERVER_PORT);
-            } catch (Exception exception) 
-            {
+            } catch (Exception exception) {
                 exception.printStackTrace();
             }
-            
-            
+
+
             new DB().go(args);
             System.out.println("DB finished");
-        } 
-        catch (SQLException ex) 
-        {
+        } catch (SQLException ex) {
             Logger.getLogger(Launcher.class.getName()).log(Level.SEVERE, null, ex);
         }
 
