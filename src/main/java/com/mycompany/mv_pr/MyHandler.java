68d026597bcedcbc33c492407021d1facbbcb568
masshhka <mashka-life@mail.ru>
2013-09-02 14:59:50 +0400

    (((

--- src/main/java/com/mycompany/mv_pr/MyHandler.java
+++ src/main/java/com/mycompany/mv_pr/MyHandler.java
@@ -8,25 +8,25 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.mortbay.jetty.Request;
-import org.mortbay.jetty.handler.AbstractHandler;
+
 
 /**
  *
  * @author user
  */
  public class MyHandler extends AbstractHandler{
-     Override
-    public void handle(String target, Request baseRequest,HttpServletRequest request, HttpServletResponse response)
-                throws IOException, ServletException {
- 
-            response.setContentType("text/html;charset=utf-8");
 
-            response.setStatus(HttpServletResponse.SC_OK);
-
-            baseRequest.setHandled(true);
-
-            response.getWriter().println("Hello World");
-        }
+{
+@Override 
+public void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch)
+        throws IOException, ServletException;
+//            response.setContentType("text/html;charset=utf-8");
+//
+//            response.setStatus(HttpServletResponse.SC_OK);
+//
+//            baseRequest.setHandled(true);
+//
+//            response.getWriter().println("Hello World");
+}
 
          }
