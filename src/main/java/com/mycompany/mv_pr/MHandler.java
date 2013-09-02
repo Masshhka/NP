5ff6db6f6d9aaf1b8730fa791a4a727c1919aed9
masshhka <mashka-life@mail.ru>
2013-09-02 12:29:27 +0400

    Handler

---
+++ src/main/java/com/mycompany/mv_pr/MHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mv_pr;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.mortbay.jetty.Request;
+
+/**
+ *
+ * @author user
+ */
+public class MHandler extends MyHandler{
+    Override
+ public void handle(String target, Request baseRequest,HttpServletRequest request, HttpServletResponse response)
+                throws IOException, ServletException {
+            
+}
